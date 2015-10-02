7aefb4a8f6ab97c531e2ce03bf0915c2ad3b446b
MICSTI <stifter.michael@gmx.net>
2015-10-02 16:33:15 +0200

    Started frontend development

--- server/view/public/js/webrtc.js
+++ server/view/public/js/webrtc.js
@@ -1,7 +1,9 @@
-$(function() {
+$(document).ready(function() {
+	// references to input elements
 	var chat = $("#chat");
 	var input = $("#message");
 	var username = $("#username");
+	var mail = $("#mail");
 	var send = $("#send");
 	
 	// if user is running mozilla then use it's built-in WebSocket
@@ -18,19 +20,26 @@ $(function() {
 	// open connection
     var connection = new WebSocket('ws://127.0.0.1:1337');
 
-    connection.onopen = function () {
-       console.log("Connection opened");
+    connection.onopen = function() {
+       console.log("WebSocket connection opened");
     };
 
-    connection.onerror = function (error) {
-        // just in there were some problems with conenction...
+    connection.onerror = function(error) {
+        // just in there were some problems with connection...
         chat.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
                                     + 'connection or the server is down.' } ));
     };
 
     // most important part - incoming messages
-    connection.onmessage = function (message) {
-        console.log(message);
+    connection.onmessage = function(message) {
+		// message origin
+		var origin = message.origin;
+		
+		// parse message JSON data
+		var data = JSON.parse(message.data);
+		
+		// call onmessage handler
+		onMessageReceived(getMessageObject(data));
     };
 	
 	// submit button
@@ -39,4 +48,45 @@ $(function() {
 		
 		input.val("");
 	});
+	
+	/**
+		Handler that will be executed when a new message is received.
+	*/
+	var onMessageReceived = function(message) {
+		chat.append(getMessageHtml(message));
+	}
+	
+	var getMessageObject = function(messageJson) {
+		var messageObject = {};
+		
+		// timestamp
+		messageObject.timestamp = new Date();
+		
+		// sender
+		// TODO: remove fake data
+		messageObject.sender = {
+			id: 0,
+			name: "Server"
+		};
+		
+		// content
+		messageObject.content = messageJson.message;
+		
+		return messageObject;
+	}
+	
+	/**
+		Returns the HTML content for a message.
+	*/
+	var getMessageHtml = function(message) {
+		var html = "";
+		
+		html += "<div class='message'>";
+			html += "<div class='message-timestamp'>" + message.timestamp + "</div>";
+			html += "<div class='message-sender'>" + message.sender.name + "</div>";
+			html += "<div class='message-content'>" + message.content + "</div>";
+		html += "</div>";
+		
+		return html;
+	}
 });

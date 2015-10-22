0197271f2971a0da63093b6316539b621f9d51b1
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-10-22 15:27:45 +0200

    Added MediaStreamTrack.getSources methods to UserMedia class

--- server/view/public/js/user-media.js
+++ server/view/public/js/user-media.js
@@ -2,10 +2,66 @@
 	This class handles the access to navigator.getUserMedia call.
 */
 var UserMedia = function() {
+	var self = this;
+		
 	/**
 		getUserMedia is depending on the browser's implementation.
 	*/
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+	/**
+		Flag to keep track if media sources have been determined.
+	*/
+	this.gotSources = false;
+
+	/**
+		Available video sources on the device.
+	*/
+	var videoSources = [];
+	
+	/**
+		Available audio sources on the device.
+	*/
+	var audioSources = [];
+	
+	/**
+		Returns an object containing the audio and video sources for the device.
+		The function returns null if the sources have not been determined yet.
+	*/
+	this.getMediaSources = function() {
+		if (!this.gotSources) {
+			return null;
+		}
+		
+		return {
+			audio: audioSources,
+			video: videoSources
+		}
+	}
+	
+	/**
+		Returns the video sources for the device.
+		The function returns null if the sources have not been determined yet.
+	*/
+	this.getVideoSources = function() {
+		if (this.gotSources) {
+			return videoSources;
+		}
+		
+		return null;
+	}
+	
+	/**
+		Returns the audio sources for the device.
+		The function return null if the sources have not been determined yet.
+	*/
+	this.getAudioSources = function() {
+		if (this.gotSources) {
+			return audioSources;
+		}
+		
+		return null;
+	}
 	
 	/**
 		Video element.
@@ -35,6 +91,7 @@ var UserMedia = function() {
 	/**
 		Inits getUserMedia.
 	*/
+	
 	this.init = function(options) {
 		this.constraints = options.constraints || null;
 		this.onSuccess = options.onSuccess || null;
@@ -55,4 +112,36 @@ var UserMedia = function() {
 		// try to get user media
 		navigator.getUserMedia(this.constraints, this.onSuccess, this.onError);
 	}
+	
+	/**
+		Gets all sources that are present on the device.
+	*/
+	var getSources = function(onSuccess) {	
+		MediaStreamTrack.getSources(function(sources) {
+			sources.forEach(function(item, idx) {
+				switch (item.kind) {
+					case "audio":
+						audioSources.push(item);
+						break;
+					
+					case "video":
+						videoSources.push(item);
+						break;
+						
+					default:
+						break;
+				}
+			});
+			
+			// set got sources flag to true
+			self.gotSources = true;
+			
+			// call onSuccess handler
+			if (onSuccess !== undefined && typeof onSuccess === 'function')
+				onSuccess(self.getMediaSources);
+		});
+	};
+	
+	// determine all video sources
+	getSources();
 }
